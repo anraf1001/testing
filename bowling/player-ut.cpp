45e242d0f44af1c0b77f5af5ab1ed84f75cd986d
Kasper Biegun <kasper.biegun95@gmail.com>
2020-08-01 17:16:20 +0200

    1.4 Testing scenario - game with strike in the middle - failed

--- bowling/player-ut.cpp
+++ bowling/player-ut.cpp
@@ -1,3 +1,16 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+
 #include "player.hpp"
+
+struct TestPlayer : public ::testing::Test {
+    Player player;
+    std::vector<std::pair<int, int>> score{};
+};
+
+TEST_F(TestPlayer, checkScoreGameWithStrikeInTheMiddle) {
+    score = {{1, 1}, {1, 1}, {1, 1}, {1, 1}, {10, 0}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}};
+    player.countScore(score);
+    ASSERT_EQ(player.getScore(), 30);
+}
--- bowling/player.cpp
+++ bowling/player.cpp
@@ -1,1 +1,18 @@
 #include "player.hpp"
+
+int Player::getScore() const {
+    return score_;
+}
+
+void Player::setScore(int score) {
+    score_ = score;
+}
+
+void Player::countScore(std::vector<std::pair<int, int>> score) {
+    int result = 0;
+    for (const auto& it : score) {
+        result += it.first;
+        result += it.second;
+    }
+    Player::setScore(result);
+}

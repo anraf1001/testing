e6c557643e83adf6211ab8aa5c2aba4966a136cc
Kasper Biegun <kasper.biegun95@gmail.com>
2020-08-01 17:29:50 +0200

    1.5 Testing scenario - game with spare in the middle - passed

--- bowling/player.cpp
+++ bowling/player.cpp
@@ -9,21 +9,40 @@ void Player::setScore(int score) {
 }
 
 void Player::countScore(std::vector<std::pair<int, int>> score) {
+    bool strike = false;
+    bool spare = false;
     int result = 0;
     int single_result = 0;
-    int multiplier = 1;
+
     for (const auto& it : score) {
         single_result = 0;
-        single_result += it.first;
-        single_result += it.second;
-        single_result *= multiplier;
+        if (strike) {
+            strike = false;
+            single_result += it.first;
+            single_result += it.second;
+            single_result *= 2;
 
-        result += single_result;
+            result += single_result;
+        } else if (spare) {
+            spare = false;
+            single_result += it.first;
+            single_result *= 2;
+            single_result += it.second;
 
-        multiplier = 1;
+            result += single_result;
+        } else {
+            single_result += it.first;
+            single_result += it.second;
+
+            result += single_result;
+        }
 
         if (it.first == 10) {
-            multiplier = 2;
+            strike = true;
+        }
+
+        if ((it.first + it.second == 10) && (it.first != 10)) {
+            spare = true;
         }
     }
     Player::setScore(result);

e96264d4ea7e1e8c59406ef9d8c1676395ec82e2
Kasper Biegun <kasper.biegun95@gmail.com>
2020-08-01 18:32:56 +0200

    Missing tests added from another PR - all passing

--- bowling/player-ut.cpp
+++ bowling/player-ut.cpp
@@ -9,6 +9,28 @@ struct TestPlayer : public ::testing::Test {
     std::vector<std::pair<int, int>> score{};
 };
 
+TEST_F(TestPlayer, checkScoreGameOfAllMisses) {
+    for (auto it = 0; it < 10; it++) {
+        score.emplace_back(0, 0);
+    }
+    player.countScore(score);
+    ASSERT_EQ(player.getScore(), 0);
+}
+
+TEST_F(TestPlayer, checkScoreGameOfAllOnes) {
+    for (auto it = 0; it < 10; it++) {
+        score.emplace_back(1, 1);
+    }
+    player.countScore(score);
+    ASSERT_EQ(player.getScore(), 20);
+}
+
+TEST_F(TestPlayer, checkScoreGameWithoutSpareAndStrike) {
+    score = {{1, 5}, {2, 5}, {4, 5}, {1, 3}, {9, 0}, {5, 4}, {4, 4}, {1, 5}, {0, 5}, {0, 9}};
+    player.countScore(score);
+    ASSERT_EQ(player.getScore(), 72);
+}
+
 TEST_F(TestPlayer, checkScoreGameWithStrikeInTheMiddle) {
     score = {{1, 1}, {1, 1}, {1, 1}, {1, 1}, {10, 0}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}};
     player.countScore(score);

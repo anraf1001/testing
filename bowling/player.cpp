2ed279e524653a972e319cff4087f7fec9baea92
Kasper Biegun <kasper.biegun95@gmail.com>
2020-08-01 17:16:56 +0200

    1.4 Testing scenario - game with strike in the middle - passed

--- bowling/player.cpp
+++ bowling/player.cpp
@@ -10,9 +10,21 @@ void Player::setScore(int score) {
 
 void Player::countScore(std::vector<std::pair<int, int>> score) {
     int result = 0;
+    int single_result = 0;
+    int multiplier = 1;
     for (const auto& it : score) {
-        result += it.first;
-        result += it.second;
+        single_result = 0;
+        single_result += it.first;
+        single_result += it.second;
+        single_result *= multiplier;
+
+        result += single_result;
+
+        multiplier = 1;
+
+        if (it.first == 10) {
+            multiplier = 2;
+        }
     }
     Player::setScore(result);
 }

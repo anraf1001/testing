8a967e5b509204f1f4598e29ef3fa6ec7f83266a
Kasper Biegun <kasper.biegun95@gmail.com>
2020-08-08 09:31:56 +0200

    Added translation of results string - tests failed

--- bowling/file.cpp
+++ bowling/file.cpp
@@ -1,1 +1,7 @@
 #include "file.hpp"
+
+std::vector<std::pair<int, int>> File::getScores() const {
+    return score_;
+}
+
+void File::translateResultsToScoreVector(std::string results) {}
--- bowling/file.hpp
+++ bowling/file.hpp
@@ -1,3 +1,14 @@
 #pragma once
 
-class File {};
+#include <string>
+#include <vector>
+
+class File {
+public:
+    std::vector<std::pair<int, int>> getScores() const;
+    void translateResultsToScoreVector(std::string results);
+
+private:
+    std::string results;
+    std::vector<std::pair<int, int>> score_;
+};
